7d64a4f838a69ae08fdedda402ffea902335209a
Eilith404 <minaya.sofia@gmail.com>
2026-01-28 13:32:58 +0100

    Add average and median calculation functions

--- statistics.js
+++ statistics.js
@@ -33,3 +33,36 @@ function calculateAverage(list) {
 }
 
 console.log(calculateAverage([10, 22, 53, 14, 10000]));
+
+/* ----------------------------------
+            MEDIAN
+---------------------------------- */
+
+// Returns true if the list has an even number of elements
+function isEven(list) {
+  return !(list.length % 2);
+}
+console.log(isEven([1, 2, 3, 4]));
+console.log(isEven([10, 22, 53]));
+
+// Returns true if the list has an odd number of elements
+function isOdd(list) {
+  return !!(list.length % 2);
+}
+console.log(isOdd([1, 2, 3]));
+console.log(isOdd([1, 2, 3, 4]));
+
+function calculateMedian(list) {
+  const listIsEven = isEven(list);
+
+  if (listIsEven) {
+  } else {
+    const middleIndex = Math.floor(list.length / 2);
+    const medianOddList = list[middleIndex];
+
+    console.log(middleIndex);
+    console.log(medianOddList);
+
+    return medianOddList;
+  }
+}
